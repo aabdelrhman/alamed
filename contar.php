c9b8469c4e48ee2c49cc38ac008f6f3a828279da
aabdelrhman <70432309+aabdelrhman@users.noreply.github.com>
2021-04-04 15:16:53 +0200

    first

---
+++ contar.php
@@ -0,0 +1,18 @@
+<?php
+
+              $conn = new mysqli("sql300.eb2a.com","eb2a_26538785","QAZxsw550","eb2a_26538785_ALameed") or die(mysqli_error());
+                if(isset($_POST['submit'])){
+                  $name = $_POST['name'];
+                  $email = $_POST['email'];
+                  $subject = $_POST['subject'];
+                  $message = $_POST['message'];
+                  $sql = "INSERT INTO clients (client_name , client_email , client_subject , client_message) VALUE ('$name' , '$email' , '$subject' , '$message')";
+                  $res = mysqli_query($conn , $sql);
+                  if(!$res){
+                    session_start();
+                    header('location:contactar.php?error=error');
+                  }else{
+                    header('location:contactar.php?good=good');
+                  }
+                }
+              ?>
